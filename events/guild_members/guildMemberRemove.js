96d1b47c82a20d7402b6fbdbce85f88459b9a1a8
Guscraftin-Dev <augustin.glorian@outlook.com>
2022-06-22 12:45:40 +0200

    Ajout log join/leave

--- events/guild_members/guildMemberRemove.js
+++ events/guild_members/guildMemberRemove.js
@@ -2,23 +2,26 @@ const { MessageEmbed, Formatters } = require('discord.js');
 const dayjs = require('dayjs');
 
 module.exports = {
-    name: 'guildMemberAdd',
+    name: 'guildMemberRemove',
     once: false,
     async execute(client, member){
         const accountCreationTimestamp = Formatters.time(dayjs(member.user.createdTimestamp).unix(), Formatters.TimestampStyles.ShortDateTime);
         const relativeAccountCreationTimestamp = Formatters.time(dayjs(member.user.createdTimestamp).unix(), Formatters.TimestampStyles.RelativeTime);
         const accountJoinTimestamp = Formatters.time(dayjs(member.joinedTimestamp).unix(), Formatters.TimestampStyles.ShortDateTime);
         const relativeAccountJoinTimestamp = Formatters.time(dayjs(member.joinedTimestamp).unix(), Formatters.TimestampStyles.RelativeTime);
+        const accountLeftTimestamp = Formatters.time(dayjs().unix(), Formatters.TimestampStyles.ShortDateTime);
+        const relativeAccountLeftTimestamp = Formatters.time(dayjs().unix(), Formatters.TimestampStyles.RelativeTime);
 
         const embed = new MessageEmbed()
             .setAuthor({ name: `${member.user.tag} (${member.id})`, iconURL: member.user.displayAvatarURL() })
-            .setColor('#21ff81')
-            .setDescription(`± Nom d'utilisateur : ${member}
+            .setColor('#dc143c')
+            .setDescription(`± Nom d'utilisateur : ${member.displayName}
             ± Créé le : ${accountCreationTimestamp} (${relativeAccountCreationTimestamp})
             ± Rejoint le : ${accountJoinTimestamp} (${relativeAccountJoinTimestamp})
+            ± Quitté le : ${accountLeftTimestamp} (${relativeAccountLeftTimestamp})
             `)
             .setTimestamp()
-            .setFooter({ text: "L'utilisateur a rejoint !" })
+            .setFooter({ text: "L'utilisateur a quitté !" })
 
         const logChannel = client.channels.cache.get('989109926200762409');
         logChannel.send({ embeds: [embed] });
