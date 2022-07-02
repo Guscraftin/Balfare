a72aca1959469c526c8489963442d94452530b64
Guscraftin <augustin.glorian@outlook.com>
2022-07-02 15:25:05 +0200

    Update Status + Total number of servers and users

--- events/client/ready.js
+++ events/client/ready.js
@@ -4,7 +4,12 @@ module.exports = {
     name: 'ready',
     once: true,
     async execute(client){
-        Logger.client("- prêt à être utilisé !");
+        let guildsCount = await client.guilds.fetch();
+        let usersCount = client.guilds.cache.reduce((a, g) => a + g.memberCount, 0);
+
+        Logger.client(`- prêt à être utilisé par ${usersCount} utilisateurs sur ${guildsCount.size} serveurs !`);
+
+        client.user.setPresence({ activities: [{ name: `mon développement`, type: 'WATCHING' }], status: 'dnd' });
 
         const devGuild = await client.guilds.cache.get('746002648506826793');
         devGuild.commands.set(client.commands.map(cmd => cmd));
