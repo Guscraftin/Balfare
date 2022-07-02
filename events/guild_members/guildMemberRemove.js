384de3a2bd0df007f8bdce479eeb8c13d6becb34
Guscraftin <augustin.glorian@outlook.com>
2022-07-02 15:03:41 +0200

    Added server log recovery

--- events/guild_members/guildMemberRemove.js
+++ events/guild_members/guildMemberRemove.js
@@ -4,6 +4,17 @@ module.exports = {
     name: 'guildMemberRemove',
     once: false,
     async execute(client, member){
+        const fetchKickLog = await member.guild.fetchAuditLogs({
+            limit: 1,
+            type: 'MEMBER_KICK'
+        });
+
+        const kickLog = fetchKickLog.entries.first();
+        const { target, reason } = kickLog;
+        let isMemberKick = false;
+
+        if (target.id === member.id) isMemberKick = true;
+
         const embed = new MessageEmbed()
             .setAuthor({ name: `${member.user.tag} (${member.id})`, iconURL: member.user.displayAvatarURL() })
             .setColor('#dc143c')
@@ -11,6 +22,7 @@ module.exports = {
             • Créé le : <t:${parseInt(member.user.createdTimestamp / 1000)}:f> (<t:${parseInt(member.user.createdTimestamp / 1000)}:R>)
             • Rejoint le : <t:${parseInt(member.joinedTimestamp / 1000)}:f> (<t:${parseInt(member.joinedTimestamp / 1000)}:R>)
             • Quitté le : <t:${parseInt(Date.now() / 1000)}:f> (<t:${parseInt(Date.now() / 1000)}:R>)
+            • Kick ? : ${isMemberKick ? `🟢 (raison : ${reason})` : `🔴`}
             `)
             .setTimestamp()
             .setFooter({ text: "L'utilisateur a quitté !" })
