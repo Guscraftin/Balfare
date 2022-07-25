d20a97770dd2b6ae80fc11ca67267d56c41a7636
Guscraftin-Dev <augustin.glorian@outlook.com>
2022-07-25 15:06:38 +0200

    Fix OwnerOnly Message

--- events/client/interactionCreate.js
+++ events/client/interactionCreate.js
@@ -17,7 +17,7 @@ module.exports = {
             if (!cmd) return interaction.reply("Cette commande n'existe pas !");
 
             if (cmd.ownerOnly) {
-                if (interaction.user.id != ownerId) return MessageChannel.reply("La seule personne pouvant taper cette commande est l'owner du bot !");
+                if (interaction.user.id != ownerId) return interaction.reply("La seule personne pouvant taper cette commande est l'owner du bot !");
             }
 
             if (!interaction.member.permissions.has([cmd.permissions])) return interaction.reply({ content: `Vous n'avez pas la/les permission(s) requise(s) (\`${cmd.permissions.join(', ')}\`) pour taper cette commande !`, ephemeral: true });
