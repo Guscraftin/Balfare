a05e60d6b1cbc7fcf1815fde451aa2b0c9ef353e
Guscraftin-Dev <augustin.glorian@outlook.com>
2022-08-11 10:17:38 +0200

    Adding logs for vocals

--- events/guild_voice_states/voiceStateUpdate.js
+++ events/guild_voice_states/voiceStateUpdate.js
@@ -1,5 +1,8 @@
 const { EmbedBuilder } = require('discord.js');
 
+// Ajouter demande de la parole dans les Stages
+// ${oldState.requestToSpeakTimestamp !== 0 && newState.requestToSpeakTimestamp === null ? `> ✋ **Demande la parole dans Stage :** \`Oui\` => \`Non\`\n` : ``} ${oldState.requestToSpeakTimestamp === null && newState.requestToSpeakTimestamp !== 0 ? `> ✋ **Demande la parole dans Stage :** \`Non\` => \`Oui\`` : ``}
+
 module.exports = {
     name: 'voiceStateUpdate',
     once: false,
@@ -7,7 +10,6 @@ module.exports = {
         const fetchGuild = await client.getGuild(newState.guild);
         const logChannel = client.channels.cache.get(fetchGuild.logChannel);
 
-        // Quand (rejoint - Leave) - change channel - Mute / Unmute - Sourdine / Unsourdine
         if (oldState.channel === null) {
             const embedJoin = new EmbedBuilder()
                 .setAuthor({ name: newState.member.user.tag, iconURL: newState.member.displayAvatarURL() })
@@ -30,6 +32,30 @@ module.exports = {
 
             logChannel.send({ embeds: [embedLeave] });
         
-        }
+        } else if (oldState.channelId != newState.channelId) {
+            const embedMove = new EmbedBuilder()
+                .setAuthor({ name: oldState.member.user.tag, iconURL: oldState.member.displayAvatarURL() })
+                .setColor('#009ECA')
+                .setDescription(`<@${oldState.member.id}> **a changé de salon vocal ${oldState.channel} => ${newState.channel}.**
+                ||(\`${oldState.channel.name}\` => \`${newState.channel.name}\`)||
+                `)
+                .setTimestamp()
+                .setFooter({ text: oldState.guild.name, iconURL: oldState.guild.iconURL() })
+
+            logChannel.send({ embeds: [embedMove] });
+
+        } else {
+            const embedDeaf = new EmbedBuilder()
+                .setAuthor({ name: oldState.member.user.tag, iconURL: oldState.member.displayAvatarURL() })
+                .setColor('#009ECA')
+                .setDescription(`**Le statut du micro de <@${oldState.member.id}> à été mis à jour dans le salon vocal ${oldState.channel} ||(\`${oldState.channel.name}\`)||.**
+                ${!oldState.selfMute && newState.selfMute ? `> 🎙 **Muet :** \`Désactivé\` => \`Activé\`\n` : ``} ${oldState.selfMute && !newState.selfMute ? `> 🎙 **Muet :** \`Activé\` => \`Désactivé\`\n` : ``} ${!oldState.selfDeaf && newState.selfDeaf ? `> 🔈 **Sourd :** \`Désactivé\` => \`Activé\`\n` : ``} ${oldState.selfDeaf && !newState.selfDeaf ? `> 🔈 **Sourd :** \`Activé\` => \`Désactivé\`\n` : ``} ${!oldState.serverMute && newState.serverMute ? `> 🎙 **Muet par un modo :** \`Désactivé\` => \`Activé\`\n` : ``} ${oldState.serverMute && !newState.serverMute ? `> 🎙 **Muet par un modo :** \`Activé\` => \`Désactivé\`\n` : ``} ${!oldState.serverDeaf && newState.serverDeaf ? `> 🔈 **Sourd par un modo :** \`Désactivé\` => \`Activé\`\n` : ``} ${oldState.serverDeaf && !newState.serverDeaf ? `> 🔈 **Sourd par un modo :** \`Activé\` => \`Désactivé\`\n` : ``} ${!oldState.selfVideo && newState.selfVideo ? `> 📷 **Camera :** \`Désactivé\` => \`Activé\`\n` : ``} ${oldState.selfVideo && !newState.selfVideo ? `> 📷 **Camera :** \`Activé\` => \`Désactivé\`\n` : ``} ${!oldState.streaming && newState.streaming ? `> 📺 **Partage d'écran :** \`Désactivé\` => \`Activé\`\n` : ``} ${oldState.streaming && !newState.streaming ? `> 📺 **Partage d'écran :** \`Activé\` => \`Désactivé\`\n` : ``} ${!oldState.suppress && newState.suppress ? `> 📣 **Muet dans Stage :** \`Désactivé\` => \`Activé\`\n` : ``} ${oldState.suppress && !newState.suppress ? `> 📣 **Muet dans Stage :** \`Activé\` => \`Désactivé\`\n` : ``}
+                `)
+                .setTimestamp()
+                .setFooter({ text: oldState.guild.name, iconURL: oldState.guild.iconURL() })
+
+            logChannel.send({ embeds: [embedDeaf] });
+
+        } 
     }
 };
