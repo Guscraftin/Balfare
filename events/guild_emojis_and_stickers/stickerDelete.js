8452fd42b9a1f232d44b6e62601e5b37614f907b
Guscraftin-Dev <augustin.glorian@outlook.com>
2022-08-12 12:07:35 +0200

    Adding ticker creation and deletion logs

---
+++ events/guild_emojis_and_stickers/stickerDelete.js
@@ -0,0 +1,23 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'stickerDelete',
+    once: false,
+    async execute(client, sticker){
+        const fetchGuild = await client.getGuild(sticker.guild);
+        const logChannel = client.channels.cache.get(fetchGuild.logChannel);
+
+        const embed = new EmbedBuilder()
+            .setTitle(`Supression d'un sticker`)
+            .setColor('#009ECA')
+            .setDescription(`**Le sticker \`${sticker.name}\` a été supprimé.**
+            ${sticker.description !== '' ? `> **Emoji similaire :** :${sticker.tags}:` : `` }
+            ${sticker.description !== '' ? `>>> **Description :** ${sticker.description}` : `` }
+            `)
+            .setImage(sticker.url)
+            .setTimestamp()
+            .setFooter({ text: sticker.guild.name, iconURL: sticker.guild.iconURL() })
+
+        logChannel.send({ embeds: [embed] });
+    }
+};
