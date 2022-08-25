01bd04547d691f8bd51124d292e7fb0e4c689858
Guscraftin-Dev <augustin.glorian@outlook.com>
2022-08-25 21:52:51 +0200

    Adding ReactionRemove Log

---
+++ events/guild_message_reactions/messageReactionRemove.js
@@ -0,0 +1,20 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'messageReactionRemove',
+    once: false,
+    async execute(client, messageReaction, user){
+        const fetchGuild = await client.getGuild(messageReaction.message.guild);
+        const logChannel = client.channels.cache.get(fetchGuild.logChannel);
+        
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: user.username, iconURL: user.displayAvatarURL() })
+            .setColor('#009ECA')
+            .setDescription(`**<@${user.id}> a retiré sa réaction \`${messageReaction.emoji.name}\` [de ce message](${messageReaction.message.url}).**
+            `)
+            .setTimestamp()
+            .setFooter({ text: messageReaction.message.guild.name, iconURL: messageReaction.message.guild.iconURL() })
+
+        logChannel.send({ embeds: [embed] });
+    }
+};
