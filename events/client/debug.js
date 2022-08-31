65513d6db2548e262a9e0de689502db566f5ebba
Guscraftin-Dev <augustin.glorian@outlook.com>
2022-08-31 15:30:46 +0200

    Add debug Log

--- events/client/debug.js
+++ events/client/debug.js
@@ -1,7 +1,7 @@
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    name: 'warn',
+    name: 'debug',
     once: false,
     async execute(client, info){
         const warnLogChannel = client.channels.cache.get('1014524532515680256');
@@ -9,7 +9,7 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setAuthor({ name: client.user.tag, iconURL: client.user.displayAvatarURL() })
             .setColor('#32c400')
-            .setDescription(`**Info warn : ** \`${info}\``)
+            .setDescription(`**Info debug : ** \`${info}\``)
             .setTimestamp()
     
         warnLogChannel.send({ embeds: [embed] });
