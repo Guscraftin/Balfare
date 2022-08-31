8dd8a38ebacc90080f027f4aea06004ca4e63d40
Guscraftin-Dev <augustin.glorian@outlook.com>
2022-08-31 15:16:45 +0200

    Update webhookUpdate.js

--- events/guild_webhooks/webhookUpdate.js
+++ events/guild_webhooks/webhookUpdate.js
@@ -1,23 +1,29 @@
 const { EmbedBuilder } = require('discord.js');
 
-// Nombre de webhook dans le salon (avec promise)
+// Détails des changements du webhooks
 
 module.exports = {
     name: 'webhookUpdate',
     once: false,
     async execute(client, channel){
         const fetchGuild = await client.getGuild(channel.guild);
+        const logChannel = client.channels.cache.get(fetchGuild.logChannel);
+
+        let numberWebhooks;
+        await channel.fetchWebhooks().then(function (hooks) {
+            numberWebhooks = hooks.size;
+        });
 
         const embed = new EmbedBuilder()
             .setTitle(`Webhook : Mise à jour`)
             .setColor('#009ECA')
-            .setDescription(`Un \`webhook\` a été **mis à jour** dans le salon ${resultChannelType()} <#${channel.id}> ||(\`${channel.name}\`)||.`)
+            .setDescription(`Un \`webhook\` a été **mis à jour** dans le salon ${resultChannelType()} <#${channel.id}> ||(\`${channel.name}\`)||.
+            > **Nombre de webhooks dans le salon :** \`${numberWebhooks}\`
+            `)
             .setTimestamp()
             .setFooter({ text: channel.guild.name, iconURL: channel.guild.iconURL() })
 
-        const logChannel = client.channels.cache.get(fetchGuild.logChannel);
         logChannel.send({ embeds: [embed] });
-
 
         function resultChannelType() {
             switch (channel.type) {
