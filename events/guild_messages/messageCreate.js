b234953fe820ecf41e279af9bd5107b1bfa1f7d0
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-03-19 09:48:58 +0000

    fix: messageCreate - add delay reaction

--- events/guild_messages/messageCreate.js
+++ events/guild_messages/messageCreate.js
@@ -5,8 +5,8 @@ module.exports = {
         
         // Add reaction in #saviez-vous in LSDC server
         if (message.channel.id === '820761893652135936') {
-            message.react('<:Pouce_bleu:784544070957268992>');
-            message.react('<:Pouce_rouge:784544048814751744>');
+            await message.react('<:Pouce_bleu:784544070957268992>');
+            await message.react('<:Pouce_rouge:784544048814751744>');
         }
     }
 };
