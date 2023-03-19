a49a27046ed759c17b3f8ccc454f41725ef9e859
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-03-19 10:49:12 +0000

    feature: add pin/unpin cmd

---
+++ commands/moderation/pin.js
@@ -0,0 +1,34 @@
+const { ApplicationCommandOptionType } = require('discord.js');
+
+module.exports = {
+    name: 'pin',
+    category: 'moderation',
+    permissions: ['SEND_MESSAGES'],
+    ownerOnly: false,
+    usage: 'pin [messageId]',
+    examples: ['pin 1086943757330555050'],
+    description: 'Epingler un message.',
+    options: [
+        {
+            name: 'message_id',
+            description: "Entrez l'id du message à désépingler",
+            type: ApplicationCommandOptionType.String,
+            required: true
+        }
+    ],
+    async runInteraction (client, interaction) {
+        /*
+         * Pin a message without permission in specific guild
+         */
+        const messageId = interaction.options.getString('messageid');
+        
+        // Only available in "Dorset Dublin S4" guild
+        if (interaction.guildId !== "1052648075501510736") return interaction.reply({ content: "Cette commande n'est pas disponible sur ce serveur !", ephemeral: true });
+
+        const message = await interaction.channel.messages.fetch(messageId).catch(() => {});
+        if (!message) return interaction.reply({ content: "Le message n'a pas été trouvé !", ephemeral: true });
+        if (message.pinned) return interaction.reply({ content: "Le message est déjà épinglé !", ephemeral: true });
+
+        return message.pin().then(interaction.reply({ content: `Le [message](${message.url}) a bien été épinglé !`, ephemeral: true }));
+    }
+};
---
+++ commands/moderation/unpin.js
@@ -0,0 +1,34 @@
+const { ApplicationCommandOptionType } = require('discord.js');
+
+module.exports = {
+    name: 'unpin',
+    category: 'moderation',
+    permissions: ['SEND_MESSAGES'],
+    ownerOnly: false,
+    usage: 'unpin [messageId]',
+    examples: ['unpin 1086943757330555050'],
+    description: 'Désépingler un message.',
+    options: [
+        {
+            name: 'message_id',
+            description: "Entrez l'id du message à épingler",
+            type: ApplicationCommandOptionType.String,
+            required: true
+        }
+    ],
+    async runInteraction (client, interaction) {
+        /*
+         * Unpin a message without permission in specific guild
+         */
+        const messageId = interaction.options.getString('messageid');
+        
+        // Only available in "Dorset Dublin S4" guild
+        if (interaction.guildId !== "1052648075501510736") return interaction.reply({ content: "Cette commande n'est pas disponible sur ce serveur !", ephemeral: true });
+
+        const message = await interaction.channel.messages.fetch(messageId).catch(() => {});
+        if (!message) return interaction.reply({ content: "Le message n'a pas été trouvé !", ephemeral: true });
+        if (!message.pinned) return interaction.reply({ content: "Le message est déjà désépinglé !", ephemeral: true });
+
+        return message.unpin().then((message) => interaction.reply({ content: `Le [message](${message.url}) a bien été désépinglé !`, ephemeral: true }));
+    }
+};
