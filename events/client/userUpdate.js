d76eb97fae61f162338ec391ddc00bf98dee2a2f
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-05-22 17:21:45 +0100

    fix: update discord.js + remove userUpdate.js

--- events/client/userUpdate.js
+++ events/client/userUpdate.js
@@ -1,25 +1,27 @@
-const { EmbedBuilder } = require('discord.js');
+// const { EmbedBuilder } = require('discord.js');
 
-// Non test car c'est le compte utilisateur qui doit changer
-// avatar - banner - Flags (badge)
-// Utile tag avec Discriminant + Peusdo ?
+// // Non test car c'est le compte utilisateur qui doit changer
+// // avatar - banner - Flags (badge)
+// // Utile tag avec Discriminant + Peusdo ?
 
-module.exports = {
-    name: 'userUpdate',
-    once: false,
-    async execute(client, oldUser, newUser){
-        const fetchGuild = await client.getGuild(newUser.guild);
-        const logChannel = client.channels.cache.get(fetchGuild.logChannel);
-        if (logChannel == undefined) return;
+// // Nouveau système de pseudo !!!
 
-        const embed = new EmbedBuilder()
-            .setAuthor({ name: newUser.tag, iconURL: newUser.displayAvatarURL() })
-            .setColor('#009ECA')
-            .setDescription(`<@${newUser.id}> **a mis à jour son compte.**
-            ${oldUser.discriminator !== newUser.discriminator ? `> **Discriminant :** \`${oldUser.discriminator}\` => \`${newUser.discriminator}\`\n` : ``} ${oldUser.tag !== newUser.tag ? `> **Tag :** \`${oldUser.tag}\` => \`${newUser.tag}\`\n` : ``} ${oldUser.username !== newUser.username ? `> **Pseudo :** \`${oldUser.username}\` => \`${newUser.username}\`\n` : ``}
-            `)
-            .setTimestamp()
+// module.exports = {
+//     name: 'userUpdate',
+//     once: false,
+//     async execute(client, oldUser, newUser){
+//         const fetchGuild = await client.getGuild(newUser.guild);
+//         const logChannel = client.channels.cache.get(fetchGuild.logChannel);
+//         if (logChannel == undefined) return;
 
-        logChannel.send({ embeds: [embed] });
-    }
-};
+//         const embed = new EmbedBuilder()
+//             .setAuthor({ name: newUser.tag, iconURL: newUser.displayAvatarURL() })
+//             .setColor('#009ECA')
+//             .setDescription(`<@${newUser.id}> **a mis à jour son compte.**
+//             ${oldUser.discriminator !== newUser.discriminator ? `> **Discriminant :** \`${oldUser.discriminator}\` => \`${newUser.discriminator}\`\n` : ``} ${oldUser.tag !== newUser.tag ? `> **Tag :** \`${oldUser.tag}\` => \`${newUser.tag}\`\n` : ``} ${oldUser.username !== newUser.username ? `> **Pseudo :** \`${oldUser.username}\` => \`${newUser.username}\`\n` : ``}
+//             `)
+//             .setTimestamp()
+
+//         logChannel.send({ embeds: [embed] });
+//     }
+// };
